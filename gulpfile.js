75ee3e731fad76bc781711ebe62aaac947c1aee8
NikitaPO <gigzim345@gmail.com>
2020-12-16 23:00:48 +0300

    concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-function defaultTask(cb) {
-  console.log("Hello");
-  cb();
+"use strict";
+
+const { src, dest } = require("gulp");
+const scss = require("gulp-sass");
+const concat = require("gulp-concat");
+// scss.compiler = require("sass");
+
+function styles() {
+  return src("app/scss/style.scss")
+    .pipe(scss({ outputStyle: "compressed" }).on("error", scss.logError))
+    .pipe(dest("dist/css/"));
 }
 
-exports.default = defaultTask;
+exports.styles = styles;
